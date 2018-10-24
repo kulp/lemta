dd19854cfdf1232ab2ed2ea5131c3f55ee6335a1
Darren Kulp <darren@kulp.ch>
2018-10-23 20:10:52 -0400

    Add some more inferred methods

--- model.h
+++ model.h
@@ -1,10 +1,22 @@
 /* Inferred from libattiny*.so */
-class Model_core;
+class Model_core
+{
+public:
+    void *readVerilogSig(char const*, unsigned long*);
+    void *getIntProperty(int, unsigned long*, char const*);
+    void *core_getIntProperty(int, unsigned long*);
+};
 
 class Model_device
 {
 public:
     Model_core *getCore(unsigned int index);
+    void cycle(unsigned int count);
+};
+
+class Avr8 : public Model_core
+{
+
 };
 
 extern "C" int model_api_ver();
