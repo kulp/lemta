a63b4658c78611e5350f221fd560499cc67d4b84
Darren Kulp <darren@kulp.ch>
2018-10-23 20:11:23 -0400

    Demonstrate a few more calls

--- main.cc
+++ main.cc
@@ -22,6 +22,19 @@ int main()
     Model_core *mc = dev->getCore(0);
     assert(("model core exists", mc != NULL));
 
+    dev->cycle(1);
+
+    unsigned long ul = -1;
+    void *result;
+    // Avr8:
+    // ; 0x400 -> 0x0
+    // ; 0x404 -> 0x20
+    // ; 0x405 -> 0x0
+    // ; 0x406 -> 0x1
+    // ; else  -> failure
+    result = mc->getIntProperty(0x404, &ul, "testy");
+    std::printf("result = %p, ul = %lu\n", result, ul);
+
     int rc = model_dtor(dev);
     assert(("destructor succeeded", rc == 0));
 
