e182e98f54f9f9673dce11b8ba08c29e435ea986
Darren Kulp <darren@kulp.ch>
2018-11-03 11:41:40 -0400

    Making calls from main dynamic
    Use parameter packs
    
    Refactoring Library / Calls

--- test/main.cc
+++ test/main.cc
@@ -4,20 +4,61 @@
 #include <cassert>
 #include <cstdio>
 
+#include <dlfcn.h>
+
 #include "model.hh"
 
-int main()
+class Library
 {
-    std::printf("svDpiVersion() = %s\n", svDpiVersion());
-    std::printf("model_api_ver() = %#x\n", model_api_ver());
+public:
+    void *handle;
 
-    Model *dev = model_ctor("ATtiny1616");
+    template<typename FunctionPtr>
+    class Calls
+    {
+        Library &lib;
+        const char *name;
+    public:
+        Calls(Library &lib, const char *name) : lib(lib), name(name) { }
+        template<typename ...Params>
+        auto operator()(Params... p) -> decltype(static_cast<FunctionPtr>(0)(p...))
+        {
+            return reinterpret_cast<FunctionPtr>(dlsym(lib.handle, name))(p...);
+        }
+    };
+
+    Library(const char *path)
+        : handle(dlopen(path, RTLD_LAZY | RTLD_LOCAL))
+    {
+        // no body
+    }
+
+    #define get_function(Func) get_function_(static_cast<decltype(Func)*>(0),#Func)
+    template<typename F>
+    auto get_function_(F *, const char *name) -> Calls<F*>
+    {
+        return Calls<F*>(*this, name);
+    }
+};
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return __LINE__;
+
+    Library lib(argv[1]);
+
+    std::printf("svDpiVersion() = %s\n", lib.get_function(svDpiVersion)());
+    std::printf("model_api_ver() = %#x\n", lib.get_function(model_api_ver)());
+
+    Model *dev = lib.get_function(model_ctor)("attiny1616");
     assert(("device constructed", dev != NULL));
 
-    svScope scope = svGetScopeFromName("TOP.sim_top");
+    svScope scope = lib.get_function(svGetScopeFromName)("TOP.sim_top");
     assert(("found scope", scope != NULL));
 
-    static_cast<VerilatedScope*>(scope)->scopeDump();
+    // cannot call C++ methods if we do not know their manglings
+    //static_cast<VerilatedScope*>(scope)->scopeDump();
 
     Core *mc = dev->getCore(0);
     assert(("model core exists", mc != NULL));
@@ -38,7 +79,7 @@ int main()
     result = mc->getIntProperty(0x404, &ul, "testy");
     std::printf("result = %p, ul = %lu\n", result, ul);
 
-    int rc = model_dtor(dev);
+    int rc = lib.get_function(model_dtor)(dev);
     assert(("destructor succeeded", rc == 0));
 
     return 0;
