4ed53be5ae141e5570d9cc27a0b039f415d99a39
Darren Kulp <darren@kulp.ch>
2018-11-15 22:13:15 -0500

    Move model_verilated to internals

--- include/interface.hh
+++ include/interface.hh
@@ -78,47 +78,6 @@ public:
     virtual int test(int, Test*, int (*)(Model*)) = 0;
 };
 
-class model_verilated
-{
-public:
-    virtual ~model_verilated();
-
-    virtual UnknownType eval() = 0;
-    virtual UnknownType dpiGetLifetimeCounter(unsigned int*) = 0;
-    virtual UnknownType dpiSetLifetimeCounter(unsigned int const*) = 0;
-    virtual UnknownType dpiGetSignature(unsigned int const*) = 0;
-    virtual UnknownType dpiGetSib(unsigned int*) = 0;
-    virtual UnknownType dpiSetDevSel(unsigned int const*, unsigned int const*) = 0;
-    virtual UnknownType dpiTick(unsigned int*) = 0;
-    virtual UnknownType dpiGetTimeStep() = 0;
-    virtual UnknownType dpiGetMainClk() = 0;
-    virtual UnknownType dpiGetPC(int) = 0;
-    virtual UnknownType dpiGetInstr(int) = 0;
-    virtual UnknownType dpiGetSP(int) = 0;
-    virtual UnknownType dpiGetSreg(int) = 0;
-    virtual UnknownType dpiPeekReg(unsigned int const*, int) = 0;
-    virtual UnknownType dpiPokeReg(unsigned int const*, unsigned int const*, int) = 0;
-    virtual UnknownType dpiSetPC(unsigned int const*, int) = 0;
-    virtual UnknownType dpiSetSP(unsigned int const*, int) = 0;
-    virtual UnknownType dpiSetSreg(unsigned int const*, int) = 0;
-    virtual UnknownType dpiGetCycleCounter(unsigned int*, int) = 0;
-    virtual UnknownType dpiSetCycleCounter(unsigned int const*, int) = 0;
-    virtual UnknownType dpiStartReset(unsigned int const*, int) = 0;
-    virtual UnknownType dpiStopReset(int) = 0;
-    virtual UnknownType dpiPeekMemory(unsigned int const*, unsigned int*, unsigned int const*, unsigned int*, int) = 0;
-    virtual UnknownType dpiPokeMemory(unsigned int const*, unsigned int*, unsigned int const*, unsigned int const*, int) = 0;
-    virtual UnknownType dpiPeekFuses(unsigned int const*) = 0;
-    virtual UnknownType dpiPokeFuses(unsigned int const*, unsigned int const*) = 0;
-    virtual UnknownType dpiGetSupportedAccessType(unsigned int const*, int) = 0;
-    virtual UnknownType dpiCheckWatchHit(unsigned int const*, unsigned int const*, unsigned int*, unsigned int const*, int) = 0;
-    virtual UnknownType dpiGetModelState(int) = 0;
-    virtual UnknownType dpiInstrFinished(int) = 0;
-};
-
-// Vsim_top multiply inherits from model_verilated and from VerilatedModule
-// Presumably model_verilated is the pure-virtual interface exposed through the
-// DLL API/ABI, and the rest of the Verilator implementation is left opaque.
-
 typedef int model_api_ver_func();
 typedef Model *model_ctor_func(const char *);
 typedef int model_dtor_func(Model *);
