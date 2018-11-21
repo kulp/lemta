bdd532c98c81cc9aaf59400729beb3d340326535
Darren Kulp <darren@kulp.ch>
2018-11-21 15:31:23 -0700

    Print out pointer in correct place

--- test/gen.lua
+++ test/gen.lua
@@ -8,28 +8,29 @@ local document = xmlua.XML.parse(xml)
 
 local classes = document:search("//Class");
 
-function elaborate_type(doc,id)
+function elaborate_type(doc,id,ptr)
     local t = doc:search("//*[@id='" .. id .. "']") -- must succeed
     --print("look up id " .. id .. " in doc " .. tostring(doc) .. " = " .. tostring(t))
+    ptr = ptr or ""
 
     for i,t in ipairs(t) do
         local name = t:name()
         local typ = t:get_attribute("type")
 
         if name == "FundamentalType" then
-            return t:get_attribute("name")
+            return t:get_attribute("name") .. ptr
         elseif name == "Class" or name == "Struct" then
-            return t:get_attribute("name")
+            return t:get_attribute("name") .. ptr
         elseif name == "CvQualifiedType" then
-            return "const " .. elaborate_type(doc,typ)
+            return "const " .. elaborate_type(doc,typ,ptr)
         elseif name == "PointerType" then
-            return elaborate_type(doc,typ) .. "*"
+            return elaborate_type(doc,typ,"*" .. ptr)
         elseif name == "ReferenceType" then
-            return elaborate_type(doc,typ) .. "&"
+            return elaborate_type(doc,typ,"&" .. ptr)
         elseif name == "Typedef" then
-            return elaborate_type(doc,typ)
+            return elaborate_type(doc,typ,ptr)
         elseif name == "FunctionType" then
-            local start = elaborate_type(doc,t:get_attribute("returns")) .. " /*function*/ ("
+            local start = elaborate_type(doc,t:get_attribute("returns")) .. "(" .. ptr .. ") ("
             local first = true
             for i, arg in ipairs(t:children()) do
                 if not first then
