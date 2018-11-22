38a9560ee6832c3379c7a55a3d296d1ca60b3c59
Darren Kulp <darren@kulp.ch>
2018-11-22 06:52:44 -0800

    Generalize loading for both Model and Core

--- test/load.lua
+++ test/load.lua
@@ -1,26 +1,34 @@
 local ffi = require("ffi")
-local stem = "Model"
+local Proto = {}
 
-local impl = ffi.load("impl-" .. stem)
+for _,stem in ipairs({ "Model", "Core" }) do
+    Proto[stem] = {}
+    local proto = Proto[stem]
+    local impl = ffi.load("impl-" .. stem)
+    local get_h = "get_header_" .. stem .. "_h_"
+    local get_m = "get_methods_" .. stem .. "_txt_"
+    ffi.cdef("const char * " .. get_h .. "();");
+    ffi.cdef("const char * " .. get_m .. "();");
+    ffi.cdef(ffi.string(impl[get_h]()))
+
+    -- create Lua entry points for C wrappers for C++ virtual calls
+    for f in string.gmatch(ffi.string(impl[get_m]()), "%S+") do
+        proto[f] = function(self, ...)
+            return impl[stem .. "__" .. f](self._ud, ...)
+        end
+    end
+end
+
 local blackbox = ffi.load("attinyxc")
-ffi.cdef("const char * get_header_" .. stem .. "_h_();");
-ffi.cdef("const char * get_methods_" .. stem .. "_txt_();");
-ffi.cdef(ffi.string(impl.get_header_Model_h_()))
 ffi.cdef[[
 int model_api_ver();
 Model *model_ctor(const char *);
 int model_dtor(Model *);
 ]]
 
-local model = {}
+local model = Proto.Model
 model._ud  = blackbox.model_ctor("")
 model._ver = blackbox.model_api_ver()
 ffi.gc(model._ud, blackbox.model_dtor)
-
-for f in string.gmatch(ffi.string(impl.get_methods_Model_txt_()), "%S+") do
-    model[f] = function(self, ...)
-        return impl["Model__" .. f](self._ud, ...)
-    end
-end
 
 model:stop()
