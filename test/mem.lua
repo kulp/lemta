24dc5f73838b867f340c93ec563318d37c98c225
Darren Kulp <darren@kulp.ch>
2018-11-26 05:44:59 -0700

    Add basic memory test

---
+++ test/mem.lua
@@ -0,0 +1,23 @@
+local ffi = require("ffi")
+local Model = require("model")
+
+local model = Model:create("attinyxc", "ATtiny1617")
+local core = model:getCore(0)
+
+-- raw read/write interface
+local size = 3
+local input  = ffi.new("unsigned char[?]", size, { 9, 8, 7 })
+local output = ffi.new("unsigned char[?]", size, { 0, 0, 0 })
+local segment = 0
+core:writeMemory(0x102, size, input, segment)
+core:readMemory( 0x102, size, output, segment)
+
+ffi.cdef[[int memcmp(const void *s1, const void *s2, size_t n);]]
+
+local handle = error
+if ffi.C.memcmp(input,output,size) == 0 then
+    handle = print
+end
+
+handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
+
