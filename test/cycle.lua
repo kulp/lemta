ccde19b3fa093b371306aa3f3e44c2b4dfb786bc
Darren Kulp <darren@kulp.ch>
2018-11-28 16:01:43 -0700

    Add test for cycle callback

---
+++ test/cycle.lua
@@ -0,0 +1,22 @@
+local ffi = require("ffi")
+local Model = require("model")
+
+local model = Model:create(unpack(arg))
+
+local cycles = 0
+local count = 10
+
+local function cycle_cb(model, userdata)
+    cycles = cycles + 1
+end
+
+model:addCycleCallback(cycle_cb, nil)
+model:cycle(count)
+
+local handle = error
+if cycles == count then
+    handle = print
+end
+
+handle("expected = " .. count .. ", actual = " .. cycles)
+
