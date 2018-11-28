8a72d615ef4ad749b42701b3807fd6b29ff0cfb5
Darren Kulp <darren@kulp.ch>
2018-11-28 06:49:06 -0700

    Hoist var for hardcoded address

--- test/mem.lua
+++ test/mem.lua
@@ -6,11 +6,12 @@ local core = model:getCore(0)
 
 -- raw read/write interface
 local size = 3
+local addr = 0x102
 local input  = ffi.new("unsigned char[?]", size, { 9, 8, 7 })
 local output = ffi.new("unsigned char[?]", size, { 0, 0, 0 })
 local segment = 0
-core:writeMemory(0x102, size, input, segment)
-core:readMemory( 0x102, size, output, segment)
+core:writeMemory(addr, size, input, segment)
+core:readMemory( addr, size, output, segment)
 
 ffi.cdef[[int memcmp(const void *s1, const void *s2, size_t n);]]
 
