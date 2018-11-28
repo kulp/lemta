e0b8c36bb8a72fad26e319eee9e28f3356f82b89
Darren Kulp <darren@kulp.ch>
2018-11-28 16:16:58 -0700

    Add testcase for getCore and getModel

---
+++ test/getters.lua
@@ -0,0 +1,14 @@
+local ffi = require("ffi")
+local Model = require("model")
+
+local model = Model:create(unpack(arg))
+local core = model:getCore(0)
+local m2 = core:getModel()
+
+local handle = error
+if model._ud == m2._ud then
+    handle = print
+end
+
+handle("expected = " .. tostring(model._ud) .. ", actual = " .. tostring(m2._ud))
+
