85426daaba6e48fd0e6914d35186a5fb2b15aca8
Darren Kulp <darren@kulp.ch>
2018-11-28 16:10:15 -0700

    Rely on closures, not userdata, for callbacks

--- test/cycle.lua
+++ test/cycle.lua
@@ -6,11 +6,11 @@ local model = Model:create(unpack(arg))
 local cycles = 0
 local count = 10
 
-local function cycle_cb(model, userdata)
+local function cycle_cb(model)
     cycles = cycles + 1
 end
 
-model:addCycleCallback(cycle_cb, nil)
+model:addCycleCallback(cycle_cb)
 model:cycle(count)
 
 local handle = error
