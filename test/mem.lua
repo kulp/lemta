b70c80f885c3d82a8c31a6d7838ed11afb5d0574
Darren Kulp <darren@kulp.ch>
2018-12-02 17:20:49 -0700

    Robustify memory test

--- test/mem.lua
+++ test/mem.lua
@@ -37,3 +37,30 @@ do
     handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
 end
 
+addr = addr - 10
+core.segments[segment].write(addr, size, input)
+
+do
+    local output = ffi.new("unsigned char[?]", size, { 0, 0, 0 })
+    core:readMemory( addr, size, output, segment)
+
+    local handle = error
+    if ffi.C.memcmp(input,output,size) == 0 then
+        handle = print
+    end
+
+    handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
+end
+
+do
+    local output = ffi.new("unsigned char[?]", size, { 0, 0, 0 })
+    core.segments[segment].read(addr, size, output)
+
+    local handle = error
+    if ffi.C.memcmp(input,output,size) == 0 then
+        handle = print
+    end
+
+    handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
+end
+
