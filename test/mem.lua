83a7f24c1157e48896f41e4dfc5647a09e14606e
Darren Kulp <darren@kulp.ch>
2018-12-03 15:53:00 -0700

    Hoist common behaviors in mem.lua

--- test/mem.lua
+++ test/mem.lua
@@ -7,45 +7,38 @@ local core = model:getCore(0)
 ffi.cdef[[int memcmp(const void *s1, const void *s2, size_t n);]]
 
 local size = 3
-local addr -- set later, per test
 local segment = 0
 local input = ffi.new("unsigned char[?]", size, { 9, 8, 7 })
 
-function check_read(addr,fashion)
+function check_read(addr, fashion)
     local output = ffi.new("unsigned char[?]", size, { 0, 0, 0 })
     fashion(addr, size, output, segment)
 
     local handle = error
-    if ffi.C.memcmp(input,output,size) == 0 then
+    if ffi.C.memcmp(input, output, size) == 0 then
         handle = print
     end
 
     handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
 end
 
--- write method 1 : raw call to writeMemory
-addr = 0x102
-core:writeMemory(addr, size, input, segment)
+local write_kinds = {
+        function(a,s,m,p) core:writeMemory(a,s,m,p) end,
+        function(a,s,m,p) core.segments[p].write(a,s,m) end,
+        function(a,s,m,p) for i = 0,s-1 do core.segments[p].mem[a + i] = m[i] end end,
+    }
 
-check_read(addr,function(a,s,o,p) core:readMemory(a,s,o,p) end)
-check_read(addr,function(a,s,o,p) core.segments[p].read(a,s,o) end)
-check_read(addr,function(a,s,o,p) for i = 0,s-1 do o[i] = core.segments[p].mem[a + i] end end)
+local read_kinds = {
+        function(a,s,m,p) core:readMemory(a,s,m,p) end,
+        function(a,s,m,p) core.segments[p].read(a,s,m) end,
+        function(a,s,m,p) for i = 0,s-1 do m[i] = core.segments[p].mem[a + i] end end,
+    }
 
--- write method 2 : segment façade with .write call
-addr = addr - 10
-core.segments[segment].write(addr, size, input)
-
-check_read(addr,function(a,s,o,p) core:readMemory(a,s,o,p) end)
-check_read(addr,function(a,s,o,p) core.segments[p].read(a,s,o) end)
-check_read(addr,function(a,s,o,p) for i = 0,s-1 do o[i] = core.segments[p].mem[a + i] end end)
-
--- write method 3 : segment façade with mem façade
-addr = addr - 20
-for i = 0,size-1 do
-    core.segments[segment].mem[addr + i] = input[i]
+for i,put in ipairs(write_kinds) do
+    local addr = 0x10 + i * size
+    put(addr, size, input, segment)
+    for _,get in ipairs(read_kinds) do
+        check_read(addr, get)
+    end
 end
-
-check_read(addr,function(a,s,o,p) core:readMemory(a,s,o,p) end)
-check_read(addr,function(a,s,o,p) core.segments[p].read(a,s,o) end)
-check_read(addr,function(a,s,o,p) for i = 0,s-1 do o[i] = core.segments[p].mem[a + i] end end)
 
