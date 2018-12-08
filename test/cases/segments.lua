70676a344ae2cf5487f98b943f5bcb153a3cc8ed
Darren Kulp <darren@kulp.ch>
2018-12-08 09:18:09 -0700

    Preparing to generalize EEPROM testcase
    The EEPROM testcase should be able to apply to all segments, or at least
    SEG_PROG, SEG_DATA, and SEG_EEPROM.

---
+++ test/cases/segments.lua
@@ -0,0 +1,84 @@
+local ffi = require("ffi")
+local Model = require("model")
+
+local model = Model:create(unpack(arg))
+local core = model:getCore(0)
+local segment_names = { "SEG_EEPROM" }
+
+ffi.cdef[[int memcmp(const void *s1, const void *s2, size_t n);]]
+
+function check_read(core, segment, addr, size, input, fashion, invert)
+    local output = ffi.new("unsigned char[?]", size)
+    for i = 0,size-1 do
+        output[i] = 0xa5
+    end
+
+    fashion(core, addr, size, output, segment)
+
+    local handle = error
+    local equal = ffi.C.memcmp(input, output, size) == 0
+    local normal  = not invert and     equal
+    local flipped =     invert and not equal
+    if normal or flipped then
+        handle = print
+    end
+
+    handle("input = " .. tostring(input) .. ", output = " .. tostring(output))
+end
+
+local write_kinds = {
+        function(c,a,s,m,p) c:writeMemory(a,s,m,p) end,
+        function(c,a,s,m,p) c.segments[p].write(a,s,m) end,
+        function(c,a,s,m,p) for i = 0,s-1 do c.segments[p].mem[a + i] = m[i] end end,
+    }
+
+local read_kinds = {
+        function(c,a,s,m,p) c:readMemory(a,s,m,p) end,
+        function(c,a,s,m,p) c.segments[p].read(a,s,m) end,
+        function(c,a,s,m,p) for i = 0,s-1 do m[i] = c.segments[p].mem[a + i] end end,
+    }
+
+for _,segment_name in ipairs(segment_names) do
+    local segment = ffi.cast("Segment", segment_name)
+    local temp = ffi.new("unsigned long[1]", 0)
+    -- integer property index N*2+5 appears to be the size of segment N
+    local width = model:getIntProperty(segment * 2 + 5, temp, nil)
+    local size = tonumber(temp[0])
+    local input = ffi.new("unsigned char[?]", size * 2)
+
+    temp[0] = 0
+    -- integer property index N*2+6 appears to be the base address of segment N
+    model:getIntProperty(segment * 2 + 6, temp, nil)
+    local addr = tonumber(temp[0])
+
+    for i = 0,size*2-1 do
+        input[i] = 0xff - i
+    end
+
+    do
+        local env = os.getenv("SKIP_UPPER_BOUND_CHECK_SEGMENT_" .. segment_name)
+        if tonumber(env) ~= 1 then
+            -- ensure segment is not larger than advertised
+            for i,put in ipairs(write_kinds) do
+                put(core, addr, size + 1, input, segment)
+                for _,get in ipairs(read_kinds) do
+                    check_read(core, segment, addr, size + 1, input, get, true)
+                end
+            end
+        end
+    end
+
+    do
+        local env = os.getenv("SKIP_LOWER_BOUND_CHECK_SEGMENT_" .. segment_name)
+        if tonumber(env) ~= 1 then
+            -- ensure segment is at least as large as advertised
+            for i,put in ipairs(write_kinds) do
+                put(core, addr, size, input, segment)
+                for _,get in ipairs(read_kinds) do
+                    check_read(core, segment, addr, size, input, get)
+                end
+            end
+        end
+    end
+end
+
--- test/segment-overrides.mk
+++ test/segment-overrides.mk
@@ -1,19 +1,19 @@
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny202-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny204-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny214-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny212-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxg-ATmega808-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxg-ATmega809-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-atmegaapb-ATMEGA48PB-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny807-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny806-cases/eeprom.lua
-SEGMENT_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny804-cases/eeprom.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny202-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny204-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny214-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxb-ATtiny212-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxg-ATmega808-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxg-ATmega809-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-atmegaapb-ATMEGA48PB-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny807-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny806-cases/segments.lua
+EEPROM_UPPER_BOUND_TARGETS += check-attinyxf-ATtiny804-cases/segments.lua
 
-$(SEGMENT_UPPER_BOUND_TARGETS): export SKIP_UPPER_BOUND_CHECK_SEGMENT_SEG_EEPROM = 1
+$(EEPROM_UPPER_BOUND_TARGETS): export SKIP_UPPER_BOUND_CHECK_SEGMENT_SEG_EEPROM = 1
 
-SEGMENT_LOWER_BOUND_TARGETS += check-attinyxe-ATmega3208-cases/eeprom.lua
-SEGMENT_LOWER_BOUND_TARGETS += check-attinyxe-ATmega3209-cases/eeprom.lua
-SEGMENT_LOWER_BOUND_TARGETS += check-attinyxe-ATmega4809-cases/eeprom.lua
-SEGMENT_LOWER_BOUND_TARGETS += check-attinyxe-ATmega4808-cases/eeprom.lua
+EEPROM_LOWER_BOUND_TARGETS += check-attinyxe-ATmega3208-cases/segments.lua
+EEPROM_LOWER_BOUND_TARGETS += check-attinyxe-ATmega3209-cases/segments.lua
+EEPROM_LOWER_BOUND_TARGETS += check-attinyxe-ATmega4809-cases/segments.lua
+EEPROM_LOWER_BOUND_TARGETS += check-attinyxe-ATmega4808-cases/segments.lua
 
-$(SEGMENT_LOWER_BOUND_TARGETS): export SKIP_LOWER_BOUND_CHECK_SEGMENT_SEG_EEPROM = 1
+$(EEPROM_LOWER_BOUND_TARGETS): export SKIP_LOWER_BOUND_CHECK_SEGMENT_SEG_EEPROM = 1
