f0d3c5e3b6b04c7bfe7d3ff383637d579272104b
Darren Kulp <darren@kulp.ch>
2018-12-08 08:14:55 -0700

    Check both sides of segment size boundary

--- test/cases/eeprom.lua
+++ test/cases/eeprom.lua
@@ -11,23 +11,30 @@ local temp = ffi.new("unsigned long[1]", 0)
 local width = model:getIntProperty(9, temp, nil)
 local size = tonumber(temp[0])
 local segment = "SEG_EEPROM"
-local input = ffi.new("unsigned char[?]", size)
+local input = ffi.new("unsigned char[?]", size * 2)
 
 temp[0] = 0
 -- integer property index 10 appears to be the base address of the EEPROM in segment 2
 model:getIntProperty(10, temp, nil)
 local addr = tonumber(temp[0])
 
-for i = 0,size-1 do
+for i = 0,size*2-1 do
     input[i] = 0xff - i
 end
 
-function check_read(addr, fashion)
+function check_read(addr, size, input, fashion, invert)
     local output = ffi.new("unsigned char[?]", size)
+    for i = 0,size-1 do
+        output[i] = 0xa5
+    end
+
     fashion(addr, size, output, segment)
 
     local handle = error
-    if ffi.C.memcmp(input, output, size) == 0 then
+    local equal = ffi.C.memcmp(input, output, size) == 0
+    local normal  = not invert and     equal
+    local flipped =     invert and not equal
+    if normal or flipped then
         handle = print
     end
 
@@ -46,10 +53,19 @@ local read_kinds = {
         function(a,s,m,p) for i = 0,s-1 do m[i] = core.segments[p].mem[a + i] end end,
     }
 
+-- ensure segment is not larger than advertised
+for i,put in ipairs(write_kinds) do
+    put(addr, size + 1, input, segment)
+    for _,get in ipairs(read_kinds) do
+        check_read(addr, size + 1, input, get, true)
+    end
+end
+
+-- ensure segment is at least as large as advertised
 for i,put in ipairs(write_kinds) do
     put(addr, size, input, segment)
     for _,get in ipairs(read_kinds) do
-        check_read(addr, get)
+        check_read(addr, size, input, get)
     end
 end
 
