26cb8c1804df3319a1a7e78f51fcc10312acbb1d
Darren Kulp <darren@kulp.ch>
2018-12-13 07:08:18 -0700

    Improve the mechanism for skipping tests
    The general intent has been, and continues to be, to test whatever can be
    successfully tested, without failing on things that may not be under our
    control.
    
    Previously, overriding of Make rules enabled this behavior, but spewed ugly
    warnings from Make, and there was no effective regular structure to the
    overrides. Now, if an entire test is to be skipped, an environment variable can
    be set, whether in an overrides file or otherwise, which increases flexibility
    and provides a more regular structure to the overrides files.
    
    There is, however, no requirement that overrides files use only this means of
    skipping tests. Indeed, some overrides files may not skip tests at all.

--- test/run-overrides.mk
+++ test/run-overrides.mk
@@ -1,9 +1,6 @@
-SKIP_RUN_TARGETS += check-attiny40-ATtiny40-cases/run.lua
-SKIP_RUN_TARGETS += check-attiny10-ATtiny5-cases/run.lua
-SKIP_RUN_TARGETS += check-attiny10-ATtiny10-cases/run.lua
-SKIP_RUN_TARGETS += check-attiny10-ATtiny4-cases/run.lua
-SKIP_RUN_TARGETS += check-attiny10-ATtiny9-cases/run.lua
-SKIP_RUN_TARGETS += check-attiny20-ATtiny20-cases/run.lua
-
-$(SKIP_RUN_TARGETS):
-	$(warning Target $@ fails and has been disabled pending investigation)
+export SKIP-attiny40-ATtiny40-cases/run.lua = 1
+export SKIP-attiny10-ATtiny5-cases/run.lua = 1
+export SKIP-attiny10-ATtiny10-cases/run.lua = 1
+export SKIP-attiny10-ATtiny4-cases/run.lua = 1
+export SKIP-attiny10-ATtiny9-cases/run.lua = 1
+export SKIP-attiny20-ATtiny20-cases/run.lua = 1
