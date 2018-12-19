a527f51c14f9c57996ac0b98e3122653ca3fff11
Darren Kulp <darren@kulp.ch>
2018-12-19 15:52:24 -0700

    Update memregs case to check regs count

--- test/cases/memregs.lua
+++ test/cases/memregs.lua
@@ -4,17 +4,21 @@ local Model = require("model")
 local model = Model:create(unpack(arg))
 local core = model:getCore(0)
 local segment = core.segments["SEG_REGS"]
+local numregs = core.props[1028].int() -- magic number not yet enumerated in C interface
 
-for i = 0,31 do
-    local input = 0xff - i
-    segment.mem[i] = input
-    local output = core.regs[i]
+for j = 0,31,numregs do -- 16-register cores have their registers mapped twice
+    for i = 0,numregs-1 do
+        local r = i + j
+        local input = 0xff - r
+        segment.mem[r] = input
+        local output = core.regs[r]
 
-    local handle = error
-    if input == output then
-        handle = print
+        local handle = error
+        if input == output then
+            handle = print
+        end
+
+        handle("input = " .. input .. ", output = " .. tostring(output))
     end
-
-    handle("input = " .. input .. ", output = " .. tostring(output))
 end
 
