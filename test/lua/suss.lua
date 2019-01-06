c3173fab65536a650da8a2c1c9906fc8437e82b4
Darren Kulp <darren@kulp.ch>
2019-01-06 15:55:57 -0600

    Extract segment sizes from libs

---
+++ test/lua/suss.lua
@@ -0,0 +1,18 @@
+local ffi = require("ffi")
+local Model = require("model")
+
+local lib, chip = unpack(arg)
+local model = Model:create(lib, chip)
+local core = model:getCore(0)
+local segment_names = { "SEG_PROG", "SEG_DATA", "SEG_EEPROM" }
+
+for _, segment_name in ipairs(segment_names) do
+    local segment = ffi.cast("Segment", segment_name)
+    -- integer property index N*2+5 appears to be the size of segment N
+    local size = model.props[segment * 2 + 5].int()
+    local chip = model.props[0].string()
+    local _, _, name = string.find(lib, "lib(at%w+).so")
+    local t = { name, chip, segment_name, "SIZE" }
+    print(table.concat(t, "_") .. "=" .. size)
+end
+
