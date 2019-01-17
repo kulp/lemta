fb8908ef91aab67cef363ba538264ae48e12a6f6
Darren Kulp <darren@kulp.ch>
2019-01-17 16:55:47 -0700

    Add Test.expect_not()

--- test/lua/test.lua
+++ test/lua/test.lua
@@ -4,6 +4,10 @@ local function equal(a, b)
     return a == b
 end
 
+local function not_equal(a, b)
+    return not equal(a, b)
+end
+
 Test.dummy = function() end
 
 Test.want = function(accepter, compare, actual)
@@ -17,6 +21,10 @@ end
 
 Test.expect = function(expected, actual)
     return Test.want(equal, expected, actual)
+end
+
+Test.expect_not = function(not_expected, actual)
+    return Test.want(not_equal, not_expected, actual)
 end
 
 return Test
