cac28dd815bfdacdc1c2f2aa14e61139c7f6fe07
Darren Kulp <darren@kulp.ch>
2019-01-19 14:47:06 -0700

    Use new Core.load functionality

--- test/cases/bp0.lua
+++ test/cases/bp0.lua
@@ -1,18 +1,12 @@
 local Model = require("model/sugared").Model
-local ihex = require("ihex")
 local Test = require("test")
 local Util = require("util")
 local ffi = require("ffi")
 
 local model = Model:create(unpack(arg))
 local core = model:getCore(0)
-local prog = core.segments["SEG_PROG"]
 
-function loader(n, addr, kind, data, ck)
-    return kind == 0 and prog.write(addr, n, data)
-end
-
-ihex.read(io.lines("cases/sum.hex"), loader)
+core:load("cases/sum.hex")
 
 model:reset("RT_TYPE_0") -- otherwise instruction at address 0 is skipped
 
