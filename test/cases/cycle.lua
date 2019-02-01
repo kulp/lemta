f76a3ba285ef5c81f37794845b836d69ba00528a
Darren Kulp <darren@kulp.ch>
2019-01-31 20:53:01 -0700

    Test multiple callback styles

--- test/cases/cycle.lua
+++ test/cases/cycle.lua
@@ -1,11 +1,18 @@
-local Models = {
-    require("model/base").Model,
-    require("model/sugared").Model,
+local Test = require("test")
+
+-- cannot require them both up front because sugared actually modifies the
+-- table imported from base
+local model_paths = { "model/base", "model/sugared" }
+
+local results = {
+    { false, true },
+    { true, true },
 }
 
-for _, Model in ipairs(Models) do
-    local Test = require("test")
+function add_cb_1(model, cb) model:addCycleCallback(cb) end
+function add_cb_2(model, cb) model:addCycleCallback(cb, nil) end
 
+function run_test(Model, add_cb)
     local model = Model:create(unpack(arg))
 
     local cycles = 0
@@ -15,8 +22,17 @@ for _, Model in ipairs(Models) do
         cycles = cycles + 1
     end
 
-    model:addCycleCallback(cycle_cb, nil)
+    add_cb(model, cycle_cb)
     model:cycle(count)
 
     Test.expect(count, cycles)
 end
+
+for i, path in ipairs(model_paths) do
+    local Model = require(path).Model
+    for j, cb in ipairs({ add_cb_1, add_cb_2 }) do
+        local result = pcall(function () run_test(Model, cb) end)
+        Test.expect(results[i][j], result)
+    end
+end
+
