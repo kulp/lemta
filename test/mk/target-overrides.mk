1d7e5bce5dd48e99cd83386bd3bff5616efeee71
Darren Kulp <darren@kulp.ch>
2019-02-10 17:50:40 -0700

    Force building / checking of .hex files

--- test/mk/target-overrides.mk
+++ test/mk/target-overrides.mk
@@ -2,5 +2,10 @@ $(MCU_LIST:%=%-cases/break.lua): cases/sum.hex
 $(MCU_LIST:%=%-cases/watch.lua): cases/rw.hex
 
 FILES_clean += cases/*.hex
-cases/%.hex:
+
+# Mark all .hex files as needing to be checked always. Otherwise, because this
+# Makefile does not know the dependency graph to build those files, it will
+# think they are up-to-date even if their sources change.
+.PHONY: FORCE
+cases/%.hex: FORCE
 	$(MAKE) -C $(@D) $(@F)
