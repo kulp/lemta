dd56884f85273017de4fd57a1508f5e1d5ae8e23
Darren Kulp <darren@kulp.ch>
2019-02-13 20:02:28 -0700

    Test breaking on read from I/O segment

---
+++ test/cases/io.lua
@@ -0,0 +1,45 @@
+local Model = require("model/sugared").Model
+local Test = require("test")
+local Util = require("util")
+
+local model = Model:create(unpack(arg))
+local core = model:getCore(0)
+
+core:load("cases/io.hex")
+
+model:reset("RT_TYPE_0") -- otherwise instruction at address 0 is skipped
+
+core:cycles(0) -- reset cycles counter
+
+local io_addr = 12 -- arbitrary, determined by io.S source code
+local caught = 0
+local kind = "BP_WATCH_READ"
+local bp = core:createBreakpoint({
+        addr = io_addr,
+        addr2 = io_addr,
+        size = 1,
+        segment = "SEG_IO",
+        type = kind,
+    })
+
+bp.handler = function(_, bp)
+    caught = caught + 1
+    Test.expect(io_addr, bp.addr2)
+    Test.expect(1, bp.hitcount)
+    Test.expect(32, bp.break_pc)
+    return 1 -- 0 would mean "keep running"
+end
+id = core:addBreakpoint(bp)
+Test.expect_not(-1, id)
+Test.expect_not(0, id)
+
+local list = core:getBreakpoints(kind)
+local count = Util.null_terminated_length(list)
+Test.expect(1, count)
+
+core:run(0)
+
+Test.expect(1, caught)
+
+Test.expect(1, core:removeBreakpoint(id))
+
--- test/mk/target-overrides.mk
+++ test/mk/target-overrides.mk
@@ -1,6 +1,37 @@
 $(MCU_LIST:%=%-cases/break.lua): cases/break.hex
 $(MCU_LIST:%=%-cases/break.lua): cases/sum.hex
 $(MCU_LIST:%=%-cases/watch.lua): cases/rw.hex
+$(MCU_LIST:%=%-cases/io.lua): cases/io.hex
+$(MCU_LIST:%=%-cases/io.lua): FORCE_SKIP = 1
+
+# Only a few combinations currently work with io.S
+check-attiny10-ATtiny10-cases/io.lua: FORCE_SKIP =#off
+check-attiny10-ATtiny4-cases/io.lua: FORCE_SKIP =#off
+check-attiny10-ATtiny5-cases/io.lua: FORCE_SKIP =#off
+check-attiny10-ATtiny9-cases/io.lua: FORCE_SKIP =#off
+check-attiny20-ATtiny20-cases/io.lua: FORCE_SKIP =#off
+check-attiny40-ATtiny40-cases/io.lua: FORCE_SKIP =#off
+check-atxmega128a1u-ATxmega64A1u-cases/io.lua: FORCE_SKIP =#off
+check-atxmega128a4-ATxmega64A4U-cases/io.lua: FORCE_SKIP =#off
+check-atxmega128a4-ATxmega64D4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega128b1-ATxmega64B3-cases/io.lua: FORCE_SKIP =#off
+check-atxmega256a3u-ATxmega64A3-cases/io.lua: FORCE_SKIP =#off
+check-atxmega256a3u-ATxmega64A3u-cases/io.lua: FORCE_SKIP =#off
+check-atxmega256a3u-ATxmega64D3-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega16A4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega16A4U-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega16D4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega32A4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega32A4U-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32a4u-ATxmega32D4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32c4-ATxmega16C4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega32c4-ATxmega32C4-cases/io.lua: FORCE_SKIP =#off
+check-atxmega64c3-ATxmega32C3-cases/io.lua: FORCE_SKIP =#off
+check-atxmega64c3-ATxmega32D3-cases/io.lua: FORCE_SKIP =#off
+check-atxmega64c3-ATxmega64C3-cases/io.lua: FORCE_SKIP =#off
+check-atxmegae5-ATxmega16E5-cases/io.lua: FORCE_SKIP =#off
+check-atxmegae5-ATxmega32E5-cases/io.lua: FORCE_SKIP =#off
+check-atxmegae5-ATxmega8E5-cases/io.lua: FORCE_SKIP =#off
 
 FILES_clean += cases/*.hex
 
